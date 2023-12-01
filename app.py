6d12c055a43b50e4a70558d1f5819ef4228a92f7
anyadas11 <147773632+anyadas11@users.noreply.github.com>
2023-11-30 19:12:18 -0500

    Cloned files

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable modification tracking
+db = SQLAlchemy(app)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(128), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+@app.route('/')
+def index():
+    messages = Message.query.order_by(Message.timestamp.desc()).all()
+    # Convert timestamps
+    target_timezone = pytz.timezone('America/New_York') 
+    for message in messages:
+        # Ensure the timestamp is in UTC before converting
+        message.timestamp = message.timestamp.replace(tzinfo=pytz.utc)
+        message.timestamp = message.timestamp.astimezone(target_timezone)
+
+    return render_template('index.html', messages=messages)
+
+@app.route('/post', methods=['POST'])
+def post_message():
+    content = request.form.get('content')
+    if not content or len(content) > 128:
+        return jsonify({'status': 'error', 'message': 'Invalid message'})
+
+    new_message = Message(content=content, timestamp=datetime.utcnow().replace(tzinfo=pytz.utc))
+    db.session.add(new_message)
+    db.session.commit()
+
+    return jsonify({'status': 'success', 'message': 'Message posted'})
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=False)
